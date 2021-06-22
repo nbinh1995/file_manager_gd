4431a9f41e77c0675a9d03e8e41d89ba92455b08
nguyen binh <nguyenbinh@MacBook-Air-cua-nguyen.local>
2021-06-22 11:02:32 +0700

    fix multi domain

--- resources/views/admins/file_manager/index.blade.php
+++ resources/views/admins/file_manager/index.blade.php
@@ -3,10 +3,13 @@
 @section('title', __('FILE MANAGER'))
 @section('header_page', __('File Manager'))
 @section('content')
+@php
+    dd($_SERVER['HTTP_HOST'])
+@endphp
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12">
-            <iframe src="/manga/laravel-filemanager"
+            <iframe src="{{($_SERVER['HTTP_HOST'] == 'vozdoremon.ddns.net') ? '/laravel-filemanager' :'/manga/laravel-filemanager'}}"
                 style="width: 100%; height: 80vh; overflow: hidden; border: none;"></iframe>
         </div>
     </div>
